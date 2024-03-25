624dc5cc0e625d2b4772c74958996fbadaac2369
SFSDeco <david.kim.ung@gmail.com>
2024-03-25 19:11:06 +0100

    Initial commit

---
+++ src/main/java/com/example/tourplanner_swen/TourPlannerSwenApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourplanner_swen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourPlannerSwenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourPlannerSwenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tourplanner_swen/TourPlannerSwenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tourplanner_swen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourPlannerSwenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
